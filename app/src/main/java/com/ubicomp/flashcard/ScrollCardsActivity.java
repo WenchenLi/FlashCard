2bac659229f8cec4e5787dc9695e2ad03f2d1c20
WenchenLi <largemaths@gmail.com>
2015-09-24 21:13:57 -0400

    done with the flashcard implementation,V001

--- app/src/main/java/com/ubicomp/flashcard/ScrollCardsActivity.java
+++ app/src/main/java/com/ubicomp/flashcard/ScrollCardsActivity.java
@@ -2,9 +2,11 @@ package com.ubicomp.flashcard;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -20,35 +22,53 @@ import com.google.android.glass.widget.CardScrollAdapter;
 import com.google.android.glass.widget.CardScrollView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ScrollCardsActivity extends Activity {
     private static final String TAG = ScrollCardsActivity.class.getSimpleName();
     private boolean mVoiceMenuEnabled = true;
+    private boolean mCards_old_empty;
     private CardScrollView mCardScroller;
     private CardScrollAdapter mAdapter;
-    private boolean mAutoDelete = true;//TODO this one need to save as sharedpreferences
-    private List<CardBuilder> cards= new ArrayList<CardBuilder>();
+    private static final int SPEECH_REQUEST = 0;
+    private boolean mCreatCards = false;//TODO this one need to save as sharedpreferences
+    private List<String> mCards_content= new ArrayList<String>();
+    private List<CardBuilder> mCards = new ArrayList<CardBuilder>();
+    private List<CardBuilder> mload_card= new ArrayList<CardBuilder>();
+
     CreateCardsTask cardstask =new CreateCardsTask();
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
 
-
-        // Requests a voice menu on this activity. As for any other window feature,
-        // be sure to request this before setContentView() is called
+//        ArrayList<String> voiceResults = getIntent().getExtras()
+//                .getStringArrayList(RecognizerIntent.EXTRA_RESULTS);
+//        if(voiceResults.get(0).toLowerCase().equals("yes")){//create new mCards,TODO make it more flexible to recognize positive answer
+//            displaySpeechRecognizer();
+//            mAdapter = new CardAdapter(mCards);
+//            getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
+//            mCreatCards =true;
+//        }
+//        else {
+            // Requests a voice menu on this activity. As for any other window feature,
+            // be sure to request this before setContentView() is called
         getWindow().requestFeature(WindowUtils.FEATURE_VOICE_COMMANDS);
         //start asyntask
         cardstask.execute(getApplicationContext());
-
         //initialize UI
-        mAdapter = new CardAdapter(createLoadCard(this));
+        mload_card = createLoadCard(this);
+        mAdapter = new CardAdapter(mload_card);
+//        }
         mCardScroller = new CardScrollView(this);
         mCardScroller.setAdapter(mAdapter);
         setContentView(mCardScroller);
         setCardScrollerListener();
+
     }
+
+
     // make things
     private List<CardBuilder> createLoadCard(Context context) {
         ArrayList<CardBuilder> cards = new ArrayList<CardBuilder>();
@@ -81,6 +101,11 @@ public class ScrollCardsActivity extends Activity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
         mCardScroller.activate();
@@ -92,7 +117,22 @@ public class ScrollCardsActivity extends Activity {
         super.onPause();
     }
 
-    //tap interface
+    @Override
+    protected void onStop() {
+        super.onStop();
+//        List<String> new_cards_content = new ArrayList<String>();
+        try {
+            Collections.reverse(mCards_content);
+//            new_cards_content.addAll(mCards_content);
+//            List<String> cards_content_old = (List<String>) Util.readObjectFromFile(getApplicationContext(), "Cards.dat");
+//            new_cards_content.addAll(cards_content_old);
+            Util.witeObjectToFile(getApplicationContext(), mCards_content, "Cards.dat");
+
+        }catch (NullPointerException e){
+
+        }
+    }
+    //touch interface
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -106,15 +146,46 @@ public class ScrollCardsActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection. Menu items typically start another
         // activity, start a service, or broadcast another intent.
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int soundEffect = Sounds.SUCCESS;
         switch (item.getItemId()) {
-            case R.id.create_card:
 
+            case R.id.create_card:
+                displaySpeechRecognizer();
                 break;
             case R.id.remember_cards:
 
                 break;
-            case R.id.menu_autodelete_on:   mAutoDelete =true; break;
-            case R.id.menu_autodelete_off:   mAutoDelete = false; break;
+            case R.id.delete_card:
+                int pos =mCardScroller.getSelectedItemPosition();
+                mCards_content.remove(pos);
+                mCards.remove(pos);
+                mCardScroller.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TO save some battery,dont do animation
+//                        mCardScroller.animate(pos, CardScrollView.Animation.DELETION);
+                        mAdapter.notifyDataSetChanged();
+
+                    }
+                });
+
+                am.playSoundEffect(soundEffect);
+                break;
+            case R.id.clear_all:
+                mCards.clear();
+                mCards_content.clear();
+                mCardScroller.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TO save some battery,dont do animation
+//                        mCardScroller.animate(pos, CardScrollView.Animation.DELETION);
+                        mAdapter.notifyDataSetChanged();
+
+                    }
+                });
+                am.playSoundEffect(soundEffect);
+                break;
 
 
             default: return super.onOptionsItemSelected(item);
@@ -164,13 +235,28 @@ public class ScrollCardsActivity extends Activity {
         if (featureId == WindowUtils.FEATURE_VOICE_COMMANDS) {
             switch (item.getItemId()) {
                 case R.id.create_card:
-
+                    displaySpeechRecognizer();
                     break;
                 case R.id.remember_cards:
 
                     break;
-                case R.id.menu_autodelete_on:   mAutoDelete =true; break;
-                case R.id.menu_autodelete_off:   mAutoDelete = false; break;
+                case R.id.delete_card:
+                    int pos =mCardScroller.getSelectedItemPosition();
+                    mCards_content.remove(pos);
+                    mCards.remove(pos);
+                    mCardScroller.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //TO save some battery,dont do animation
+//                        mCardScroller.animate(pos, CardScrollView.Animation.DELETION);
+                            mAdapter.notifyDataSetChanged();
+
+                        }
+                    });
+                    AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                    int soundEffect = Sounds.SUCCESS;
+                    am.playSoundEffect(soundEffect);
+                    break;
 
 
                 default: return true;  // No change.
@@ -189,13 +275,24 @@ public class ScrollCardsActivity extends Activity {
         }
         protected List<CardBuilder> doInBackground(Context... params) {
             Log.v(TAG+"ASYN","Strat dib");
+//            try {
+//                Thread.sleep(1000 * 2);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
             try {
-                Thread.sleep(1000 * 5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                mCards_content = (List<String>) Util.readObjectFromFile(getApplicationContext(), "Cards.dat");
+                for (String text : mCards_content) {
+                    mCards.add(new CardBuilder(params[0], CardBuilder.Layout.TEXT)
+                            .setText(text));
+                }
+            }
+            catch (NullPointerException e){
+                mCards_content= new ArrayList<String>();
+
             }
             Log.v(TAG+"ASYN","end dib");
-            return cards;
+            return mCards;
         }
 
         protected void onProgressUpdate(Integer... progress) {
@@ -209,14 +306,35 @@ public class ScrollCardsActivity extends Activity {
             Log.v(TAG, "OnPostExecute");
             //temp stuff to show loading card
 
-
-            mAdapter = new CardAdapter(cards);
+            if(mCards.size()>0)mAdapter = new CardAdapter(mCards);
+            else mAdapter = new CardAdapter(mload_card);
             mCardScroller.setAdapter(mAdapter);
             // Tells the CardScrollView to activate and be ready for display.
             mCardScroller.activate();
             //redraw UI
             mCardScroller.invalidate();
         }
+    }
+
+    // speech recong part
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        startActivityForResult(intent, SPEECH_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // add spokenText to mCards.
+            mCards.add(new CardBuilder(getApplicationContext(), CardBuilder.Layout.TEXT)
+                    .setText(spokenText));
+            mCards_content.add(spokenText);
+            mAdapter.notifyDataSetChanged();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
 }
